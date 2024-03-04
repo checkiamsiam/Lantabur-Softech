e9398411af171de579d285d6969ad5e2ad5a997f
checkiamsiam <issiam02415@gmail.com>
2024-03-04 23:42:15 +0600

    Update next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-const nextConfig = withPWA({
-  output: 'standalone'
-});
-
-module.exports = nextConfig;
+export default nextConfig;
